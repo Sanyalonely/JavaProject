78e5796f3ba305d6ec3ef0a94722758647ee2719
Pilipenko Oleksandr <sanikpilipenko@gmail.com>
2026-03-26 22:37:35 +0200

    Update Main2.java

--- practice/code/Mainproject_java/src/ex03/Main2.java
+++ practice/code/Mainproject_java/src/ex03/Main2.java
@@ -15,8 +15,7 @@ public class Main2 extends Main1 {
     }
 
     public static void main(String[] args) {
-        // Використовуємо нову фабрику для створення табличного відображення
         Main2 main = new Main2(new ViewableTable().getView());
-        main.menu(); // Меню підтягнеться автоматично з ex02.Main1
+        main.menu();
     }
 }
