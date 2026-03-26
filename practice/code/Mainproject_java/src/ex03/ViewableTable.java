c5cd2fc30141397b4dbbe2df2ec109a2f15d1cd8
Pilipenko Oleksandr <sanikpilipenko@gmail.com>
2026-03-26 22:39:21 +0200

    Update ViewableTable.java

--- practice/code/Mainproject_java/src/ex03/ViewableTable.java
+++ practice/code/Mainproject_java/src/ex03/ViewableTable.java
@@ -6,7 +6,7 @@ import ex02.View;
 /**
  * Concrete Creator.
  * Оголошує метод, що "фабрикує" об'єкти.
- * @author Студент
+ * @author Пилипенко
  * @version 1.0
  */
 public class ViewableTable extends ViewableResult {
