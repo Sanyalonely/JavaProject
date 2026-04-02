b7a806f0dd96a77f9710fafd44fc6c7a268af087
Pilipenko Oleksandr <sanikpilipenko@gmail.com>
2026-04-03 00:34:58 +0300

    Update ChangeConsoleCommand.java

--- practice/code/Mainproject_java/src/ex04/ChangeConsoleCommand.java
+++ practice/code/Mainproject_java/src/ex04/ChangeConsoleCommand.java
@@ -1,8 +1,10 @@
 package ex04;
 
-import ex01.KineticEnergy;
 import ex02.View;
 import ex02.ViewResult;
+import java.io.*;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * Команда для зміни даних із можливістю відміни.
@@ -10,7 +12,7 @@ import ex02.ViewResult;
  */
 public class ChangeConsoleCommand implements ConsoleCommand {
     private View view;
-    private double lastOffset;
+    private byte[] backupState; 
 
     public ChangeConsoleCommand(View view) {
         this.view = view;
@@ -23,29 +25,51 @@ public class ChangeConsoleCommand implements ConsoleCommand {
 
     @Override
     public String toString() {
-        return "'c' change";
+        return "'c'hange";
     }
 
     @Override
     public void execute() {
-        lastOffset = Math.random() * 100.0;
-        System.out.println("Change item: add offset " + lastOffset);
+        System.out.println("Change executed...");
         if (view instanceof ViewResult) {
-            for (KineticEnergy item : ((ViewResult) view).getItems()) {
-                item.setEnergy(item.getEnergy() + lastOffset);
+            try {
+                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                ObjectOutputStream oos = new ObjectOutputStream(baos);
+                oos.writeObject(((ViewResult) view).getItems());
+                backupState = baos.toByteArray();
+
+                ViewResult viewResult = (ViewResult) view;
+                Collections.reverse(viewResult.getItems());
+                System.out.println("Table order reversed!");
+
+            } catch (IOException e) {
+                System.err.println("Помилка створення бекапу: " + e);
             }
         }
         view.viewShow();
     }
 
+    @SuppressWarnings({"unchecked", "rawtypes"})
     @Override
     public void undo() {
-        System.out.println("Undo change: subtract offset " + lastOffset);
-        if (view instanceof ViewResult) {
-            for (KineticEnergy item : ((ViewResult) view).getItems()) {
-                item.setEnergy(item.getEnergy() - lastOffset);
+        if (backupState != null && view instanceof ViewResult) {
+            try {
+                ByteArrayInputStream bais = new ByteArrayInputStream(backupState);
+                ObjectInputStream ois = new ObjectInputStream(bais);
+                List items = (List) ois.readObject();
+                
+                ViewResult viewResult = (ViewResult) view;
+                viewResult.getItems().clear();
+                viewResult.getItems().addAll(items);
+                
+                System.out.println("Last action undone. Data reverted.");
+                view.viewShow();
+
+            } catch (Exception e) {
+                System.err.println("Помилка скасування: " + e);
             }
+        } else {
+            System.out.println("Nothing to undo.");
         }
-        view.viewShow();
     }
 }
