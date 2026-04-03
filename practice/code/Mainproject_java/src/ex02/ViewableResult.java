189f8284fdd74fbbda63268b15b3204fabc622d6
Pilipenko Oleksandr <sanikpilipenko@gmail.com>
2026-04-03 13:54:17 +0300

    Update ViewableResult.java

--- practice/code/Mainproject_java/src/ex02/ViewableResult.java
+++ practice/code/Mainproject_java/src/ex02/ViewableResult.java
@@ -1,11 +1,10 @@
 package ex02;
 
-/** * Concrete Creator.
+/**
  * Реалізує метод, що "фабрикує" об'єкти.
  */
 public class ViewableResult implements Viewable {
     /** Створює відображуваний об'єкт ViewResult */
-    @Override
     public View getView() {
         return new ViewResult();
     }
